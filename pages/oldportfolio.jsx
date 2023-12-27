549f7da02d88c1f6ff2d349dc80f46a0e4db1c40
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-12-27 12:01:54 -0300

    changes in opacity

--- pages/oldportfolio.jsx
+++ pages/oldportfolio.jsx
@@ -9,9 +9,9 @@ const OldPortfolio = () => {
   return (
     <div className="w-full">
       <div className="w-screen h-[30vh] lg:w-full lg:h-[40vh] relative">
-        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/70 z-10" />
+        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10" />
         <Image
-          className="absolute z-1"
+          className="absolute z-1 "
           layout="fill"
           objectFit="cover"
           src={oldPortfolioImg}
